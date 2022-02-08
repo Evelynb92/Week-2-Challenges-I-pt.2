dcc3ebf0487a73d7513c49452331daa8eb04d275
EvelynBrooks1 <98491379+Evelynb92@users.noreply.github.com>
2022-02-08 18:00:53 +0000

    Initial commit

---
+++ main (copy).py
@@ -0,0 +1,10 @@
+FirstName = input('What is your first name?:')
+
+LastName = input('What is your last name?:')
+print(f"{FirstName[0] + LastName[0]}")
+
+
+
+FirstName = input('What is your first name?:')
+for x in range(0):
+  print(x)
---
+++ main.py
@@ -0,0 +1,5 @@
+
+FirstName = input('What is your first name?:')
+
+LastName = input('What is your last name?:')
+print(f"{FirstName[0] + LastName[0]}")
